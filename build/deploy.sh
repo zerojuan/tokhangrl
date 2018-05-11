9c0c31461b65087a5c563d539dbd61f503bc45e1
Julius Cebreros <me@juliuscebreros.com>
2018-05-11 22:17:36 +1200

    Clear bucket contents

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,5 +2,5 @@
 
 echo "Copying build to Storage"
 gsutil defacl ch -u AllUsers:READER gs://$STORAGE_BUCKET
-gsutil rm gs://$STORAGE_BUCKET
+gsutil rm gs://$STORAGE_BUCKET/**
 gsutil rsync -R /tmp/workspace/dist gs://$STORAGE_BUCKET
