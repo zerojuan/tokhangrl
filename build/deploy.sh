8fc798e44d50e76aa4945d91e01d7e9461020659
Julius Cebreros <me@juliuscebreros.com>
2018-05-14 21:45:37 +1200

    Dont cache application bundles

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,3 +3,5 @@
 echo "Copying build to Storage"
 gsutil defacl ch -u AllUsers:READER gs://$STORAGE_BUCKET
 gsutil rsync -R /tmp/workspace/dist gs://$STORAGE_BUCKET
+gsutil setmeta -h "Cache-Control:private" \
+  gs://$STORAGE_BUCKET/*.js
