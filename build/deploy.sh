75a9191a474dcba73536dcd014542c5447d9fd4c
Julius Cebreros <me@juliuscebreros.com>
2018-07-22 21:08:23 +1200

    Fix cache issue

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,5 +3,5 @@
 echo "Copying build to Storage"
 gsutil defacl ch -u AllUsers:READER gs://$STORAGE_BUCKET
 gsutil rsync -R /tmp/workspace/dist gs://$STORAGE_BUCKET
-gsutil setmeta -h "Cache-Control:private" \
+gsutil setmeta -h "Cache-Control:private, max-age=0, no-transform" \
   gs://$STORAGE_BUCKET/*.js
