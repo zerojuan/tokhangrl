3694ee33b50f09f852b94d7e5f2277306112e192
Julius Cebreros <juliuscebreros@gmail.com>
2020-02-04 22:24:47 +1300

    Add level editor scene

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import "antd/dist/antd.css";
 
 import App from "./App";
 import FlowField from "./playground/FlowField";
+import LevelEditor from "./playground/LevelEditor";
 
 ReactDOM.render(
     <BrowserRouter>
         <div>
             <Route exact path="/" component={App} />
             <Route path="/flowfield" component={FlowField} />
+            <Route path="/editor" component={LevelEditor} />
         </div>
     </BrowserRouter>,
     document.getElementById("game")
